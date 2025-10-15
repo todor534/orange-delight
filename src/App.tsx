12d6e8b1879e4e826151048745fbc086d0a7c25a
todor534 <t.todorov534@gmail.com>
2025-10-15 06:19:04 +0300

    chore: update src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import page from "../page.json";
+import { RenderBlocks } from "./blocks";
+import type { PageSchemaType } from "./types";
+export default function App(){
+  return <RenderBlocks page={page as PageSchemaType} />;
+}
